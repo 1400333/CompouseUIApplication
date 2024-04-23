551df5beff3bdadedf52b7f862964b368284d3cc
1400333 <wanrulin@softmobile.com.tw>
2024-04-23 10:53:00 +0800

    CustomButton
    CustomButton

--- app/src/main/java/com/example/compouseuiapplication/basic/Lesson3Activity.kt
+++ app/src/main/java/com/example/compouseuiapplication/basic/Lesson3Activity.kt
@@ -2,18 +2,40 @@ package com.example.compouseuiapplication.basic
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compouseuiapplication.data.Message
 import com.example.compouseuiapplication.ui.common.MessageCard.MessageCard3
 import com.example.compouseuiapplication.ui.theme.CompouseUIApplicationTheme
+import com.example.compouseuiapplication.ui.theme.CustomBtnBorder
+import com.example.compouseuiapplication.ui.theme.CustomBtnDisabled
+import com.example.compouseuiapplication.ui.theme.CustomBtnEnabled
+import com.example.compouseuiapplication.ui.theme.CustomBtnTextDisabled
+import com.example.compouseuiapplication.ui.theme.CustomBtnTextEnabled
 
 /**
  * 第 3 課：深色主題
@@ -28,11 +50,15 @@ class Lesson3Activity : ComponentActivity() {
         setContent {
             CompouseUIApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),  //全螢幕填滿
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MessageCard3(Message("停機原因","對於「00940之亂」，財訊傳媒董事長謝金河指出，從台灣的「錢多現象」來看，ETF規模還會往上升，政府該如何引導龎大游資，才是核心的問題。"))
+                Surface(modifier = Modifier.fillMaxSize(),  //全螢幕填滿
+                        color = MaterialTheme.colorScheme.background) {
+                    Column {
+                        MessageCard3(Message("停機原因",
+                                             "對於「00940之亂」，財訊傳媒董事長謝金河指出，從台灣的「錢多現象」來看，ETF規模還會往上升，政府該如何引導龎大游資，才是核心的問題。"))
+                        CustomButton(onButtonClick = {}, "測試標題")
+                        CustomButton(onButtonClick = {}, "測試標題", false)
+                    }
+
                 }
             }
         }
@@ -48,11 +74,53 @@ class Lesson3Activity : ComponentActivity() {
 @Composable
 fun PreviewLesson3() {
     CompouseUIApplicationTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            MessageCard3(Message("新聞快報","有關於「00940之亂」，財訊傳媒董事長謝金河指出，從台灣的「錢多現象」來看，ETF規模還會往上升，政府該如何引導龎大游資，才是核心的問題。"))
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Column {
+                MessageCard3(Message("新聞快報",
+                                     "有關於「00940之亂」，財訊傳媒董事長謝金河指出，從台灣的「錢多現象」來看，ETF規模還會往上升，政府該如何引導龎大游資，才是核心的問題。"))
+                CustomButton(onButtonClick = {}, "測試標題")
+                CustomButton(onButtonClick = {}, "測試標題", false)
+            }
         }
     }
+}
+
+@Composable
+fun CustomButton(
+    onButtonClick: () -> Unit,
+    text: String,
+    isEnabled: Boolean = true,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()    //Pressed、Dragged、Focused、Hovered
+
+    Log.d("RDLog", "[isEnabled]${isEnabled}[isPressed]${isPressed}")
+
+    var borderStroke: BorderStroke? = null
+    val textColot: Color
+
+    if (isEnabled) {
+        borderStroke = BorderStroke(1.dp, CustomBtnBorder())
+        textColot = CustomBtnTextEnabled()
+    } else {
+        textColot = CustomBtnTextDisabled()
+    }
+
+    Button(onClick = onButtonClick,
+           enabled = isEnabled,
+           modifier = Modifier
+               .fillMaxWidth()
+               .padding(10.dp)
+               .height(44.dp),
+           shape = RoundedCornerShape(6.dp),
+           colors = ButtonDefaults.buttonColors(containerColor = CustomBtnEnabled(),
+                                                disabledContainerColor = CustomBtnDisabled()),
+           border = borderStroke) {
+        Text(text = text,
+             modifier = Modifier.align(Alignment.CenterVertically),
+             color = textColot,
+             fontSize = 16.sp)
+
+    }
+
 }
--- app/src/main/java/com/example/compouseuiapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/compouseuiapplication/ui/theme/Color.kt
@@ -9,6 +9,9 @@ import androidx.compose.ui.graphics.Color
 //色票
 val black272727 = Color(0xFF272727)
 val black = Color(0xFF000000)
+val greyish_brown = Color(0xFF484848)
+val line_gray = Color(0xFFe0e0e0)
+val text_gray = Color(0xFF989898)
 val sky_blue = Color(0xFF66adff)
 val dark_sky_blue = Color(0xFF3a8ce9)
 val nice_blue = Color(0xFF0b57a6)
@@ -22,6 +25,56 @@ fun AppBackgroundColor(darkTheme: Boolean = isSystemInDarkTheme()): Color {
     val colorRet: Color = when {
         darkTheme -> nice_blue
         else -> white
+    }
+
+    return colorRet
+}
+
+@Composable
+fun CustomBtnEnabled(darkTheme: Boolean = isSystemInDarkTheme()): Color {
+    val colorRet: Color = when {
+        darkTheme -> greyish_brown
+        else -> white
+    }
+
+    return colorRet
+}
+
+@Composable
+fun CustomBtnDisabled(darkTheme: Boolean = isSystemInDarkTheme()): Color {
+    val colorRet: Color = when {
+        darkTheme -> greyish_brown
+        else -> line_gray
+    }
+
+    return colorRet
+}
+
+@Composable
+fun CustomBtnBorder(darkTheme: Boolean = isSystemInDarkTheme()): Color {
+    val colorRet: Color = when {
+        darkTheme -> sky_blue
+        else -> dark_sky_blue
+    }
+
+    return colorRet
+}
+
+@Composable
+fun CustomBtnTextEnabled(darkTheme: Boolean = isSystemInDarkTheme()): Color {
+    val colorRet: Color = when {
+        darkTheme -> sky_blue
+        else -> dark_sky_blue
+    }
+
+    return colorRet
+}
+
+@Composable
+fun CustomBtnTextDisabled(darkTheme: Boolean = isSystemInDarkTheme()): Color {
+    val colorRet: Color = when {
+        darkTheme -> text_gray
+        else -> text_gray
     }
 
     return colorRet
