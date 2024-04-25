ea661078828ca46b39b4209681881d22615f4ff9
1400333 <wanrulin@softmobile.com.tw>
2024-04-25 14:06:29 +0800

    showProgress
    showProgress

--- app/src/main/java/com/example/compouseuiapplication/advanced/RequestSampleActivity.kt
+++ app/src/main/java/com/example/compouseuiapplication/advanced/RequestSampleActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.example.compouseuiapplication.data.CountryData
 
 /**
@@ -23,7 +37,8 @@ class RequestSampleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Conversation(m_viewModel)
+            onLoading(m_viewModel)
+            initCountry(m_viewModel)
 
             m_viewModel.querySampleData()
         }
@@ -31,7 +46,7 @@ class RequestSampleActivity : ComponentActivity() {
 }
 
 @Composable
-fun Conversation(viewModel: RequestSampleViewModel) {
+fun initCountry(viewModel: RequestSampleViewModel) {
     val countryDataList by viewModel.countryDataList.collectAsState()
 
     LazyColumn {
@@ -44,4 +59,27 @@ fun Conversation(viewModel: RequestSampleViewModel) {
 @Composable
 fun CountryCard(countryData: CountryData) {
     Text(text = "[CountryId] ${countryData.getCountryId()}!")
+}
+
+@Composable
+fun onLoading(viewModel: RequestSampleViewModel) {
+    val isLoading by viewModel.bLoading.collectAsState()
+
+    showProgress(isLoading)
+}
+
+@Composable
+fun showProgress(bShow: Boolean) {
+    if (bShow) {
+        Dialog(onDismissRequest = { },
+               DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)) {
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(White, shape = RoundedCornerShape(8.dp))) {
+                CircularProgressIndicator()
+            }
+        }
+    }
+
 }
